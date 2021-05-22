d7c9e02d56014ed1d60b5b0bc04ec3763db4fafd
Valton Jones <jones.trae@gmail.com>
2021-05-22 10:00:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var customerName = 'bob'
+const leastFavoriteCustomer = 'trae'
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+
+
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'trae'
+  }
 // Write your solution in this file!
